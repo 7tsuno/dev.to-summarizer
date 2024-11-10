a71e56a9b3dc43f75b078319749197c75fed3b77
7tsuno <natsumeg.music@gmail.com>
2024-11-10 21:54:44 +0900

    a

---
+++ src/renderer/src/containers/pages/Settings/SettingsContainer.tsx
@@ -0,0 +1,50 @@
+// containers/SettingsContainer.tsx
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getKey, saveKey } from '@renderer/utils/store'
+
+const SettingsContainer: React.FC = () => {
+  const navigate = useNavigate()
+
+  const [apiKeys, setApiKeys] = useState({
+    devTo: '',
+    openAI: ''
+  })
+
+  useEffect(() => {
+    const loadApiKeys = async () => {
+      setApiKeys({
+        devTo: await getKey('devTo'),
+        openAI: await getKey('openAI')
+      })
+    }
+    loadApiKeys()
+  }, [])
+
+  const handleApiKeyChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target
+    setApiKeys((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const handleApiKeySave = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    saveKey('devTo', apiKeys.devTo)
+    saveKey('openAI', apiKeys.openAI)
+    navigate('/')
+  }
+
+  const handleBackClick = (): void => {
+    navigate('/')
+  }
+
+  return (
+    <SettingsComponent
+      apiKeys={apiKeys}
+      onApiKeyChange={handleApiKeyChange}
+      onApiKeySave={handleApiKeySave}
+      onBackClick={handleBackClick}
+    />
+  )
+}
+
+export default SettingsContainer
